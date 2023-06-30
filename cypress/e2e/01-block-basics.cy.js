fefb4d6cfa5bc85a6985d62889bdc256f9780cf5
ana-oprea <anaop3212@gmail.com>
2023-06-30 17:06:55 +0300

    test: add e2e tests for EditingProgress- refs #254313

--- cypress/e2e/01-block-basics.cy.js
+++ cypress/e2e/01-block-basics.cy.js
@@ -3,6 +3,24 @@ import { slateBeforeEach, slateAfterEach } from '../support/e2e';
 describe('Blocks Tests', () => {
   beforeEach(slateBeforeEach);
   afterEach(slateAfterEach);
+  const documentStep = `{
+  "Document": [
+    {
+      "condition": "python:value",
+      "hideReady": "False",
+      "iconEmpty": "eea-icon eea-icon-edit",
+      "iconReady": "eea-icon eea-icon-check",
+      "labelEmpty": "Please set the {label} of this {context.portal_type}",
+      "labelReady": "You added the {label}",
+      "link": "edit#fieldset-default-field-label-progress_test",
+      "linkLabel": "Add {label}",
+      "prefix": "progress_test",
+      "states": [
+        "all"
+      ]
+    }
+  ]
+}`;
 
   it('Add Block: Empty', () => {
     // Change page title
@@ -13,17 +31,48 @@ describe('Blocks Tests', () => {
 
     cy.getSlate().click();
 
-    // Add block
-    cy.get('.ui.basic.icon.button.block-add-button').first().click();
-    cy.get('.blocks-chooser .title').contains('Media').click();
-    cy.get('.content.active.media .button.image').contains('Image').click();
+    cy.getSlate().type('Hello World!');
 
     // Save
     cy.get('#toolbar-save').click();
     cy.url().should('eq', Cypress.config().baseUrl + '/cypress/my-page');
 
-    // then the page view should contain our changes
-    cy.contains('My Add-on Page');
-    cy.get('.block.image');
+    cy.get('#toolbar-personal').click();
+    cy.contains('Site Setup').click();
+
+    cy.contains('Editing Progress').click();
+    cy.get('#field-progress')
+      .dblclick()
+      .invoke('val', documentStep)
+      .trigger('change')
+      .click();
+
+    cy.navigate('/cypress/my-page');
+
+    cy.get('#toolbar-personal').click();
+    cy.contains('Site Setup').click();
+
+    cy.contains('Content Types').click();
+    cy.get('.ui.dropdown.actions-Document .ellipsis.horizontal.icon').click();
+
+    cy.contains('Schema').click({ force: true });
+
+    cy.get('#addfield').click();
+
+    cy.get('.ui.form .ui.input #field-title')
+      .eq(1)
+      .click()
+      .type('test_progress');
+    cy.get('#field-factory').click().type('text');
+    cy.get('.react-select__menu ').contains('Text').click({ force: true });
+    cy.get('.ui.toggle.checkbox input').click({ force: true });
+
+    cy.get('.actions button[title="Save"]').click();
+    cy.get('#toolbar-save').click();
+
+    cy.navigate('/cypress/my-page');
+
+    cy.get('#toolbar-cut-blocks').should('be.visible');
+    cy.get('#toolbar-cut-blocks').click();
   });
 });
