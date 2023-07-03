527cecbee80d2ba06fa9e5807be5006368fda726
ana-oprea <anaop3212@gmail.com>
2023-07-03 16:05:26 +0300

    test: add e2e test for ScrollIntoView - refs #254313

--- cypress/e2e/01-block-basics.cy.js
+++ cypress/e2e/01-block-basics.cy.js
@@ -12,9 +12,23 @@ describe('Blocks Tests', () => {
       "iconReady": "eea-icon eea-icon-check",
       "labelEmpty": "Please set the {label} of this {context.portal_type}",
       "labelReady": "You added the {label}",
-      "link": "edit#fieldset-default-field-label-progress_test",
+      "link": "edit#fieldset-default-field-label-test_progress",
       "linkLabel": "Add {label}",
-      "prefix": "progress_test",
+      "prefix": "test_progress",
+      "states": [
+        "all"
+      ]
+    },
+     {
+      "condition": "python:value",
+      "hideReady": "False",
+      "iconEmpty": "eea-icon eea-icon-edit",
+      "iconReady": "eea-icon eea-icon-check",
+      "labelEmpty": "Please set the {label} of this {context.portal_type}",
+      "labelReady": "You added the {label}",
+      "link": "edit#fieldset-default-field-label-test_progress_2",
+      "linkLabel": "Add {label}",
+      "prefix": "test_progress_2",
       "states": [
         "all"
       ]
@@ -57,7 +71,9 @@ describe('Blocks Tests', () => {
     cy.contains('Content Types').click();
     cy.get('.ui.dropdown.actions-Document .ellipsis.horizontal.icon').click();
 
-    cy.contains('Schema').click({ force: true });
+    cy.get('.ui.active.visible.dropdown.actions-Document .item')
+      .contains('Schema')
+      .click();
 
     cy.get('#addfield').click();
 
@@ -67,15 +83,76 @@ describe('Blocks Tests', () => {
       .type('test_progress');
     cy.get('#field-factory').click().type('text');
     cy.get('.react-select__menu ').contains('Text').click({ force: true });
-    // cy.get('.ui.toggle.checkbox input').click({ force: true });
 
     cy.get('.inline.field.field-wrapper-required input').click({ force: true });
     cy.get('.actions button[title="Save"]').click();
+
+    cy.get('.tabular.menu .item-add').click();
+    cy.get('.modal .ui.input #field-title').click().type('Test Progress');
+    cy.get('#field-id').click({ force: true }).type('testing');
+    cy.get('.actions button[aria-label="Save"]').click();
+
+    cy.get('.tabular.menu .item').contains('Test Progress').click();
+    cy.get('#addfield').click();
+
+    cy.get('.ui.form .ui.input #field-title').click().type('test_progress_2');
+    cy.get('#field-factory').click().type('text');
+    cy.get('.react-select__menu ').contains('Text').click({ force: true });
+
+    cy.get('.inline.field.field-wrapper-required input').click({
+      force: true,
+    });
+    cy.get('.actions button[title="Save"]').click();
+
     cy.get('#toolbar-save').click();
 
     cy.navigate('/cypress/my-page');
 
     cy.get('#toolbar-cut-blocks').should('be.visible');
     cy.get('#toolbar-cut-blocks').click();
+
+    cy.get('.progress__title').should('contain', '2 fields missing');
+    cy.get('.progress__title button').click();
+    cy.get('.progress__title').should('contain', 'Add test_progress');
+    cy.get('.progress__title').should('contain', 'Add test_progress_2');
+    cy.get('.progress__title ul li a').contains('Add test_progress').click();
+
+    cy.url().should('include', '#fieldset-default-field-label-test_progress');
+
+    cy.get('.field-wrapper-test_progress div[role="textbox"] p').type('test');
+    cy.get('.field-wrapper-test_progress_2 div[role="textbox"] p').type('test');
+
+    cy.get('#toolbar-save').click();
+    cy.url().should('eq', Cypress.config().baseUrl + '/cypress/my-page');
+
+    cy.get('#toolbar-personal').click();
+    cy.contains('Site Setup').click();
+
+    cy.contains('Editing Progress').click();
+    cy.get('#field-progress')
+      .dblclick()
+      .invoke('val', `{}`)
+      .click()
+      .type('{enter}');
+
+    cy.get('#toolbar-save').click();
+    cy.navigate('/cypress/my-page');
+
+    cy.get('#toolbar-personal').click();
+    cy.contains('Site Setup').click();
+
+    cy.contains('Content Types').click();
+    cy.get('.ui.dropdown.actions-Document .ellipsis.horizontal.icon').click();
+
+    cy.get('.ui.active.visible.dropdown.actions-Document .item')
+      .contains('Schema')
+      .click();
+
+    cy.get(
+      '.field-wrapper-test_progress .toolbar button[aria-label="Delete"]',
+    ).click();
+
+    cy.get('.actions .primary.button').contains('OK').click();
+    cy.get('#toolbar-save').click();
   });
 });
