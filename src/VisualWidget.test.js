237594f2f9b583ae78b3b39af4a16223d240919d
ana-oprea <anaop3212@gmail.com>
2024-03-29 20:04:15 +0200

    test: add unit tests and delete snapshots

--- src/VisualWidget.test.js
+++ src/VisualWidget.test.js
@@ -2,12 +2,16 @@ import { makeFirstLetterCapital } from './WidgetDataComponent';
 import React from 'react';
 import { Provider } from 'react-intl-redux';
 import { MemoryRouter } from 'react-router-dom';
-import renderer from 'react-test-renderer';
+import { render, screen } from '@testing-library/react';
 import configureStore from 'redux-mock-store';
-import VisualJSONWidget from './VisualJSONWidget';
 import { backgroundColor } from './WidgetSidebar';
+import '@testing-library/jest-dom/extend-expect';
+
+import VisualJSONWidget from './VisualJSONWidget';
+
 const mockStore = configureStore();
 const propsEmpty = {};
+
 describe('Widget Data Component', () => {
   it('should make first letter capital', () => {
     const testString = 'this is a test string';
@@ -16,6 +20,7 @@ describe('Widget Data Component', () => {
     );
   });
 });
+
 describe('Widget Sidebar', () => {
   it('should return a background lightblue', () => {
     expect(backgroundColor(true, false)).toEqual('lightblue');
@@ -25,6 +30,7 @@ describe('Widget Sidebar', () => {
     expect(backgroundColor(false, true)).toEqual('lightpink');
   });
 });
+
 describe('Visual widget', () => {
   it('renders the VisualJSONWidget component without breaking if props and progressEditing are empty', () => {
     const store = mockStore({
@@ -33,19 +39,38 @@ describe('Visual widget', () => {
         messages: {},
       },
       progressEditing: {},
+      rawdata: {},
+      types: {
+        loaded: true,
+        loading: false,
+        types: [
+          {
+            id: 'content-type-1',
+            title: 'Content Type 1',
+            '@id': '/content-type-1',
+          },
+        ],
+      },
     });
-    const component = renderer.create(
+    const { container } = render(
       <Provider store={store}>
         <MemoryRouter>
           <VisualJSONWidget
             pathname="/test"
             {...propsEmpty}
             hasToolbar={true}
+            value={{}}
+            onChange={jest.fn()}
           />
         </MemoryRouter>
       </Provider>,
     );
-    const json = component.toJSON();
-    expect(json).toMatchSnapshot();
+
+    expect(screen.getByText('Edit JSON')).toBeInTheDocument();
+    expect(screen.getByText('Add Property')).toBeInTheDocument();
+    expect(
+      container.querySelector('input[placeholder="Search... "]'),
+    ).toBeInTheDocument();
+    expect(screen.getByText('Content Type 1')).toBeInTheDocument();
   });
 });
